342ea0ea1c7c006d5278a18ed8227a1d2c8a710e
Denise Mello <denisemello.zero@gmail.com>
2020-12-01 18:01:22 -0300

    Criadas as classes Employee e OutsourcedEmployee

---
+++ src/application/Program.java
@@ -0,0 +1,18 @@
+package application;
+
+import entities.Employee;
+import entities.OutsourcedEmployee;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Employee x = new Employee ("Alex", 50, 20.0);
+		Employee y = new OutsourcedEmployee("Bob", 100, 15.0, 200.0);
+		
+		//logica implementada nas classes. Criar lista do tipo employee 
+		
+
+	}
+
+}
